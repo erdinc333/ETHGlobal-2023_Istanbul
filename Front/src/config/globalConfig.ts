83d42d31cbaac82cf027be9d6a0aac746e2f2d09
Arnaud <arnaud.hippo@gmail.com>
2023-11-19 03:12:11 +0100

    Initialize subgraph

--- Front/src/config/globalConfig.ts
+++ Front/src/config/globalConfig.ts
@@ -1,5 +1,5 @@
 export const contractAdresses = {
-    sepolia : '0xf31e10e16e94278d5b02842025988390e85aafd9'
+    sepolia : '0x3fa92e6259e1359de0670bee81cf8c2fd54a50ba'
 }
 // export const graphQueryUrl = 'https://api.studio.thegraph.com/query/56107/ticketprotocol/version/latest'
 export const graphQueryUrl = 'https://api.studio.thegraph.com/query/56107/ticketprotocol/v0.0.2'
