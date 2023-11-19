d9bfdc46954851763e36faf78312e2379f5cf3e6
Arnaud <arnaud.hippo@gmail.com>
2023-11-19 03:14:23 +0100

    updated smart contract and the graph

--- Front/src/config/globalConfig.ts
+++ Front/src/config/globalConfig.ts
@@ -2,4 +2,4 @@ export const contractAdresses = {
     sepolia : '0x3fa92e6259e1359de0670bee81cf8c2fd54a50ba'
 }
 // export const graphQueryUrl = 'https://api.studio.thegraph.com/query/56107/ticketprotocol/version/latest'
-export const graphQueryUrl = 'https://api.studio.thegraph.com/query/56107/ticketprotocol/v0.0.2'
+export const graphQueryUrl = 'https://api.studio.thegraph.com/query/56107/ticketprotocol/v0.0.3'
